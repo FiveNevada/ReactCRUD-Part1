cb5df7d7550d79210c3136dadd690daa9d9d2ce7
Jesus Rodriguez Barron <89615192+FiveNevada@users.noreply.github.com>
2024-03-22 23:01:25 -0600

    Delete RH-BackEnd directory

--- RH-BackEnd/src/main/java/fivenevada/rh/repositorio/EmpleadoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package fivenevada.rh.repositorio;
-
-import fivenevada.rh.modelo.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmpleadoRepositorio  extends JpaRepository<Empleado, Integer> {
-}
